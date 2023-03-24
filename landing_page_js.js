e9ee5977c618db5f60405bdc78831cf8a3565bbc
nibane <azari46165@gmail.com>
2023-03-24 15:48:01 +0100

    ajout interactiviter bouton dark mode

--- landing_page_js.js
+++ landing_page_js.js
@@ -1,10 +1,29 @@
 const scroll_top = document.getElementById('scroll_top')
-console.log(scroll_top)
 scroll_top.addEventListener('click',() => {
 
     window.scrollTo({
         top: 0,
         left: 0,
     })
+
+})
+
+const switchThemeBtn = document.querySelector('#button_dark_mode')
+let toggleTheme = 0;
+
+
+switchThemeBtn.addEventListener('click',() => {
+    console.log("ok");
+    if(toggleTheme === 0){
+        document.documentElement.style.setProperty('--ecriture', '#f1f1f1')
+        document.documentElement.style.setProperty('--background', '#262626')
+        toggleTheme++;
+        
+    }else{
+        
+        document.documentElement.style.setProperty('--ecriture', '#262626')
+        document.documentElement.style.setProperty('--background', '#f1f1f1')
+        toggleTheme--;
+    }
 
 })
